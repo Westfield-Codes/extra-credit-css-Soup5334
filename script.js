67378568c742ebdb0a4c8beea1877db4677fc2a6
Soup <izahbel209153@student-schoolsofwestfield.org>
2025-06-02 14:04:36 +0000

    nameee

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function setup(){
+    let place=document.getElementById("name");
+    let button=document.createElement("button");
+
+    button.innerHTML="Click to enter name";
+    button.addEventListener("click", addName);
+    place.appendChild(button);
+}
+function addName(){
+    let myName=prompt("enter name here");
+    let place=document.getElementById("name");
+    place.innerHTML="<h3>"+myName+"</h3>";
+}
