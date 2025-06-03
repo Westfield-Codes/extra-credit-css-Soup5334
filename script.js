f1b7ab08b53f2f163678adf857c968898c655da3
Soup <izahbel209153@student-schoolsofwestfield.org>
2025-06-03 14:05:16 +0000

    yipeeeee

--- script.js
+++ script.js
@@ -8,7 +8,20 @@ function setup(){
     place.appendChild(button);
 }
 function addName(){
-    let myName=prompt("enter name here");
+    let nameBox= document.getElementById("get-name");
+    let myName=nameBox.value;
     let place=document.getElementById("name");
     place.innerHTML="<h3>"+myName+"</h3>";
+    let play=document.createElement("button");
+    play.innerHTML="Play R-P-S";
+    play.addEventListener("click", howManyGames);
+    place.appendChild(play);
+    place.style.borderBottom="none";
+}
+
+function howManyGames(){
+let title=document.getElementById("title");
+title.innerHTML="RPS";
+let subtitle =document.getElementById("subtitle");
+subtitle.innerHTML="How Many Rounds?";
 }
